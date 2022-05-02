23f8ef890295d2b701b7e4533814bfa2887b6ebb
jadozal2 <101235726+jadozal2@users.noreply.github.com>
2022-05-01 19:02:51 -0600

    up req

--- settings.py
+++ settings.py
@@ -3,8 +3,8 @@ from os import environ
 #--------- Flask settings
 #SERVER_HOST = '0.0.0.0' # Update this for the appropriate front-end website when up
 SERVER_HOST = environ.get('SERVER_HOST', 'localhost')
-#SERVER_PORT = 5000
-SERVER_PORT = int(environ.get('SERVER_PORT', '5000'))
+SERVER_PORT = 5000
+#SERVER_PORT = int(environ.get('SERVER_PORT', '5000'))
 
 FLASK_DEBUG = False # Do not use debug mode in prod
 
