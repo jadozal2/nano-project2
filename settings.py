d9971f9d0dc8f5135f8f4c0770ab7d779c15b568
jadozal2 <101235726+jadozal2@users.noreply.github.com>
2022-05-01 18:48:56 -0600

    updated host

--- settings.py
+++ settings.py
@@ -1,7 +1,10 @@
+from os import environ
+
 #--------- Flask settings
-SERVER_HOST = '0.0.0.0' # Update this for the appropriate front-end website when up
+#SERVER_HOST = '0.0.0.0' # Update this for the appropriate front-end website when up
+SERVER_HOST = environ.get('SERVER_HOST', 'localhost')
 SERVER_PORT = 5000
-FLASK_DEBUG = True # Do not use debug mode in prod
+FLASK_DEBUG = False # Do not use debug mode in prod
 
 # Flask-Restplus settings
 SWAGGER_UI_DOC_EXPANSION = 'list'
